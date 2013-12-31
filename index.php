1ab313e2a866a65856cc1cd7627c0524047660ae
PankajAgarwal <PankajAgarwal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-31 12:04:09 +0000

    incresed version number in index  file
    git-svn-id: https://plugins.svn.wordpress.org/sliding-contact-form-by-formget/trunk@830753 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   Plugin Name: Sliding Contact Form By Formget
   Plugin URI: http://www.formget.com
   Description:  Sliding Contact Form By Formget is a advance form builder tools. This plugin let you manage your formget account from your wordpress dashboard.
-  Version: 1.0
+  Version: 1.1
   Author: FormGet
   Author URI: http://www.formget.com
  */
